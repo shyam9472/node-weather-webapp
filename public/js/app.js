94c01bc916bacd5acbfcd93915cc147ff08ce74c
Shyam <shyam2794@gmail.com>
2021-11-12 00:24:24 +0530

    Removing console log

--- public/js/app.js
+++ public/js/app.js
@@ -11,13 +11,10 @@ weatherForm.addEventListener("submit", (event) => {
   fetch('/weather?address=' + searchVal.value).then((response) => {
     response.json().then((data) => {
       if(data.error) {
-          console.log(data.error)
           messageOne.textContent = data.error
           messageTwo.textContent = ''
       }
       else {
-          console.log(data.location)
-          console.log(data.forecastData)
           messageOne.textContent = data.location
           messageTwo.textContent = data.forecastData
       }
