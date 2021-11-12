b4f2033bc1da981b89ee5dde7d1cf47f7a304978
Shyam <shyam2794@gmail.com>
2021-11-12 11:59:01 +0530

    Addition info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude , callback) => {
           callback(body.info, undefined)
       }
       else {
-          callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+          callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. Humidity will be " + body.current.humidity + ".")
       }
   })
 }
